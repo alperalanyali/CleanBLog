0b2a9e09c37c4874cfaa6995bde3b668b508cdc1
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-03-08 16:37:31 +0300

    get method is added

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
-
+app.get('/',(req,res)=>{
+    const blog = { id: 1, title: "Blog title", description: "Blog description" }
+    res.send(blog);
+})
 
 
 const port =3001;
