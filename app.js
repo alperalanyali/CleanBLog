408a8a68c4ef1596d3bb2e7083f63ef96518f5a9
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-03-16 00:35:15 +0300

    Mongo db baglandi saglandi ve  verileri cekil

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const ejs = require('ejs');
-
-
+const mongoose = require('mongoose');
+const Post = require('./models/Post')
 const app = express();
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 
+mongoose.connect("mongodb+srv://alanyalialper:1234@pcat.xdwxvcc.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("Mongoose bağlantı kuruldu")
+})
 
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get('/',async (req,res)=>{
+    const posts = await Post.find({})
+    res.render('index',{posts});
 })
 
 app.get("/about",(req,res)=>{
@@ -20,6 +30,11 @@ app.get('/add',(req,res)=>{
     res.render('add_post')
 })
 
+app.post('/add',async (req,res)=>{
+    console.log(req.body)
+    await Post.create(req.body);
+    res.redirect('/')
+})
 const port =3000;
 
 app.listen(port,()=>{
